3fea3f7fe2fc5d69c71714cbbd25aecb10712a50
Paul Molnar <paul_molnar@brown.edu>
2021-04-10 14:51:28 -0400

    visual updates

--- code/detection_mix_d.py
+++ code/detection_mix_d.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # hyper parameters
 parameter1 = 50
 parameter21 = 30
@@ -12,25 +13,30 @@ max_radius = 40
 min_offset = 10
 max_offset = 20
 
+
 # pre process image
 img = cv2.imread('../data/top/no_background/004/front.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 blur = cv2.blur(gray, (3, 3))
 
+
 # detects circles
 detected_circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 
     1, min_distance, param1 = parameter1, param2 = parameter21, minRadius = min_radius, maxRadius = max_radius)
+
 
 # find median radius of circles
 num_circles = detected_circles.shape[1]
 sorted_circles = np.sort(detected_circles[0,:,2])
 med_rad = int(sorted_circles[int(num_circles/2)])
 
+
 # redetect circles with better radius parameter
 min_rad = med_rad - min_offset
 max_rad = med_rad + max_offset
 detected_circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 
     1, min_distance, param1 = parameter1, param2 = parameter22, minRadius = min_rad, maxRadius = max_rad)
+
 
 # Methodology - want to estimate corners from detected points, know they're evenly spaced so
 num_pts = detected_circles.shape[1]
@@ -58,6 +64,7 @@ y_step = (y_max - y_min) / 10
 for i in range(1):
     cv2.line(img, (int(x_min + x_step*i), y_max), (int(x_min + x_step*i), y_min), (255, 255, 255), 2)
     cv2.line(img, (x_min, int(y_min + y_step*i)), (x_max, int(y_min + y_step*i)), (255, 255, 255), 2)
+
 
 # calculate board representation from current grid
 board = np.zeros((10,10))
@@ -96,19 +103,21 @@ for i in range(10):
         else:
             board[i,j] = 0
 
+
 # visualize the board
-canvas = np.zeros((1000,1000,3))
+canvas = np.zeros((800,800,3))
 for i in range(10):
     for j in range(10):
-        start = (50 + 100*j,50+ 100*i)
+        start = (40 + 80*j,40+ 80*i)
         if board[i,j] == 1:
-            cv2.circle(canvas, start, 50, (255, 255, 255), -1)
+            cv2.circle(canvas, start, 40, (255, 255, 255), -1)
         elif board[i,j] == 2:
-            cv2.circle(canvas, start, 50, (0, 0, 255), -1)
+            cv2.circle(canvas, start, 40, (0, 0, 255), -1)
         else:
-            cv2.circle(canvas, start, 50, (255, 0, 0), -1)
+            cv2.circle(canvas, start, 40, (255, 0, 0), -1)
 cv2.imshow("Board", canvas)
 cv2.waitKey(0)
+
 
 # show circles on image
 for pt in detected_circles[0,:]:
@@ -116,6 +125,7 @@ for pt in detected_circles[0,:]:
     cv2.circle(img, (a,b), r, (0, 255, 0), 2)
     cv2.circle(img, (a,b), 1, (0, 0, 255), 3)
 cv2.imshow("Detected Circles", img)
+
 
 # press escape to get out of image window or your terminal might bug out
 key = cv2.waitKey(0)
