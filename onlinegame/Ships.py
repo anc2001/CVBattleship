30b340d2482a3142cbb797437d80c4c3a25cd389
Devansh Gupta <devansh_gupta@brown.edu>
2021-04-11 21:21:24 -0400

    s

---
+++ onlinegame/Ships.py
@@ -0,0 +1,8 @@
+from Place import Place
+class Ships(Place):
+    
+    def show_ships(self, number_of_ships, grid_player):
+        print(" ")
+        for row in grid_player:
+            print (" ".join(row))
+        print("\nThere are",number_of_ships, "ships left to place \n")
